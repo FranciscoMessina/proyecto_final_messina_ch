64a384abb32b8d25021a9c5c87936b7abf99fe09
Tomás Messina <45271713+messina8@users.noreply.github.com>
2021-12-25 18:02:10 -0300

    Updated controller, prefab and spell

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,70 +4,117 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float sideSpeed;
+    [SerializeField] private float moveSpeed = 8;
+    [SerializeField] private float sideSpeed = 6;
     private Rigidbody _rb;
+    private CinemachineTouchInputMapper _cM;
 
     private float vInput;
     private float hInput;
 
     private Animator _anim;
 
+    private bool canShoot = true;
+    [SerializeField] private float shootDelay = 1;
+    private bool canArea = true;
+    [SerializeField] private float areaDelay = 4;
+    private float shootTimer;
+    private float areaTimer;
+
+    [SerializeField] private GameObject iceSpell;
+    [SerializeField] private float spellSpeed;
+    [SerializeField] private Transform spellSpawnPoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        GameManager.instance.GetPlayerPosition(this.transform);
         _rb = GetComponent<Rigidbody>();
         _anim = GetComponent<Animator>();
+        _cM = GetComponent<CinemachineTouchInputMapper>();
+
+
+
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
-            vInput = Input.GetAxis("Vertical") * moveSpeed;
-            hInput = Input.GetAxis("Horizontal") * sideSpeed;
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+        hInput = Input.GetAxis("Horizontal") * sideSpeed;
 
-            if (Input.GetButton("Vertical"))
-            {
-            //transform.Translate(0, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
-            //_rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.deltaTime);
 
-            if (Input.GetAxis("Vertical") == -1) { _anim.SetInteger("AnimNum", -1); }
-            else if (Input.GetAxis("Vertical") == 1) { _anim.SetInteger("AnimNum", 1); }
+        if (canShoot == false && shootTimer >= 0)
+        {
+            shootTimer -= Time.deltaTime;
         }
-
-            else if (Input.GetButton("Horizontal"))
-            {
-                //transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime, 0);
-
-                if (Input.GetAxis("Horizontal") == -1) { _anim.SetInteger("AnimNum", -2); }
-                else if (Input.GetAxis("Horizontal") == 1) { _anim.SetInteger("AnimNum", 2); }
-            }
-
-            else
-            {
-            _anim.SetInteger("AnimNum", 0);
-            }
-        
+        else if (canShoot == false && shootTimer <= 0)
+        {
+            canShoot = true;
+        }
     }
 
 
     void FixedUpdate()
     {
-        
-            //vInput = Input.GetAxis("Vertical") * moveSpeed;
-            //hInput = Input.GetAxis("Horizontal") * rotateSpeed;
+
+        Walk();
+
+        if (Input.GetButton("Fire1") && canShoot)
+        {
+            Shoot();
+            shootTimer = shootDelay;
+        }
+
+        else if (Input.GetMouseButtonDown(1) && canArea)
+        {
+            _anim.SetInteger("AnimNum", 5);
+        }
 
 
-            Vector3 rotation = Vector3.up * hInput;
 
-            Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+    }
 
-            _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+    void Shoot()
+    {
+        Invoke("CastSpell", .6f);
+        //GameObject newspell = Instantiate(iceSpell, spellSpawnPoint.position, this.transform.rotation) as GameObject;
+        _anim.SetInteger("AnimNum", 4);
+        canShoot = false;
+        Debug.Log("Shoot called");
+    }
 
-            _rb.MoveRotation(_rb.rotation * angleRot);
-        
+    private void CastSpell()
+    {
+        GameObject newspell = Instantiate(iceSpell, spellSpawnPoint.position, this.transform.rotation) as GameObject;
+        Rigidbody spellRB = newspell.GetComponent<Rigidbody>();
 
+        spellRB.velocity = this.transform.forward * spellSpeed;
+    }
+
+    void Walk()
+    {
+        if (Input.GetButton("Vertical"))
+        {
+
+            if (Input.GetAxis("Vertical") == -1) { _anim.SetInteger("AnimNum", -1); }
+            else if (Input.GetAxis("Vertical") == 1) { _anim.SetInteger("AnimNum", 1); }
+        }
+        else if (Input.GetButton("Horizontal"))
+        {
+
+
+            if (Input.GetAxis("Horizontal") == -1) { _anim.SetInteger("AnimNum", -2); }
+            else if (Input.GetAxis("Horizontal") == 1) { _anim.SetInteger("AnimNum", 2); }
+        }
+        else
+        {
+            _anim.SetInteger("AnimNum", 0);
+        }
+
+        Vector3 forward = this.transform.forward * vInput * Time.fixedDeltaTime;
+
+        Vector3 sideways = this.transform.right * hInput * Time.fixedDeltaTime;
+
+        _rb.MovePosition(this.transform.position + forward + sideways);
     }
 }
