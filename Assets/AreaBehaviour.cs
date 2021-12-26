07ae019aa0e3bd51748a1f6c4cfb8efab7a3f45f
Tomás Messina <45271713+messina8@users.noreply.github.com>
2021-12-26 08:14:56 -0300

    made new Branch to safekeep from errors

---
+++ Assets/AreaBehaviour.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaBehaviour : MonoBehaviour
+{
+    [SerializeField] private float timeOnScreen;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeOnScreen -= Time.deltaTime;
+        if (timeOnScreen <= 0)
+            Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,165 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 8;
+    [SerializeField] private float sideSpeed = 6;
+    private Rigidbody _rb;
+    [SerializeField] private Camera _cam;
+
+    private float vInput;
+    private float hInput;
+
+    private Animator _anim;
+
+    private bool canShoot = true;
+    [SerializeField] private float shootDelay = 1;
+    private bool canArea = true;
+    [SerializeField] private float areaDelay = 4;
+    private float shootTimer;
+    private float areaTimer;
+
+    [SerializeField] private GameObject iceSpell;
+    [SerializeField] private float spellSpeed;
+    [SerializeField] private Transform spellSpawnPoint;
+
+    [SerializeField] private GameObject areaSpell;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
+        //_cam = Camera.main;
+
+
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+        hInput = Input.GetAxis("Horizontal") * sideSpeed;
+
+
+        if (canShoot == false && shootTimer >= 0)
+        {
+            shootTimer -= Time.deltaTime;
+        }
+        else if (canShoot == false && shootTimer <= 0)
+        {
+            canShoot = true;
+        }
+
+        if (canArea == false && areaTimer >= 0)
+        {
+            areaTimer -= Time.deltaTime;
+        }
+        else if (canArea == false && areaTimer <= 0)
+        {
+            canArea = true;
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+
+        Walk(vInput, hInput);
+        Rotate();
+
+        //float xAxis = Input.GetAxis("Vertical");
+        //float zAxis = Input.GetAxis("Horizontal");
+
+        if (Input.GetButton("Fire1") && canShoot)
+        {
+            Shoot();
+            shootTimer = shootDelay;
+        }
+
+        else if (Input.GetButton("Fire2") && canArea)
+        {
+            Area();
+            areaTimer = areaDelay;            
+        }
+
+
+
+    }
+
+    void Shoot()
+    {
+        Invoke("CastSpell", .6f);
+        //GameObject newspell = Instantiate(iceSpell, spellSpawnPoint.position, this.transform.rotation) as GameObject;
+        _anim.SetInteger("AnimNum", 1);
+        canShoot = false;
+        Debug.Log("Shoot called");
+        //_anim.SetInteger("AnimNum", 0);
+    }
+
+    void Area()
+    {
+        Invoke("CastArea", 1f);
+        _anim.SetInteger("AnimNum", 2);
+        canArea = false;
+        Debug.Log("Area called");
+    }
+
+    private void CastSpell()
+    {
+        GameObject newspell = Instantiate(iceSpell, spellSpawnPoint.position, this.transform.rotation) as GameObject;
+        Rigidbody spellRB = newspell.GetComponent<Rigidbody>();
+
+        spellRB.velocity = this.transform.forward * spellSpeed;
+    }
+
+    private void CastArea()
+    {
+        GameObject newarea = Instantiate(areaSpell, this.transform.position, this.transform.rotation) as GameObject;
+    }
+
+    void Rotate()
+    {
+        Quaternion CharacterRotation = _cam.transform.rotation;
+        CharacterRotation.x = 0;
+        CharacterRotation.z = 0;
+
+        this.transform.rotation = CharacterRotation;
+    }
+
+    void Walk(float x, float z)
+    {
+
+        _anim.SetFloat("MoveX", x);
+        _anim.SetFloat("MoveZ", z);
+
+
+        /*if (Input.GetButton("Vertical"))
+        {
+
+            if (Input.GetAxis("Vertical") == -1) { _anim.SetInteger("MoveX", -1); }
+            else if (Input.GetAxis("Vertical") == 1) { _anim.SetInteger("MoveX", 1); }
+        }
+        else if (Input.GetButton("Horizontal"))
+        {
+
+
+            if (Input.GetAxis("Horizontal") == -1) { _anim.SetInteger("MoveZ", -1); }
+            else if (Input.GetAxis("Horizontal") == 1) { _anim.SetInteger("MoveZ", 1); }
+        }
+        else
+        {
+            _anim.SetInteger("MoveX", 0);
+            _anim.SetInteger("Movez", 0);
+        }*/
+
+        Vector3 forward = this.transform.forward * vInput * Time.fixedDeltaTime;
+
+        Vector3 sideways = this.transform.right * hInput * Time.fixedDeltaTime;
+
+        _rb.MovePosition(this.transform.position + forward + sideways);
+    }
+}
