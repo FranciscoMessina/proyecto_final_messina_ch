091602af3b8804852b6d981d949e3ced265f309e
Tomás Messina <45271713+messina8@users.noreply.github.com>
2021-12-26 17:02:57 -0300

    added spin anim for Caster

--- Assets/Scripts/Caster.cs
+++ Assets/Scripts/Caster.cs
@@ -6,11 +6,12 @@ public class Caster : MonoBehaviour
 {
     [SerializeField] private Transform target; 
     [SerializeField] private float rotationSpeed = 5;
+    private Animator _anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,7 +25,13 @@ public class Caster : MonoBehaviour
 
         Quaternion rotation = Quaternion.LookRotation(target.position - transform.position);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        _anim.SetFloat("rotate", 1);
 
 
+    }
+
+    public void Die()
+    {
+        _anim.SetTrigger("die");
     }
 }
