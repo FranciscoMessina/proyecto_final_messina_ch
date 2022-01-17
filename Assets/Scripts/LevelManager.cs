80f4e7773aa892511a182eb56c1238f099879ed8
Tomás Messina <45271713+messina8@users.noreply.github.com>
2022-01-16 21:22:00 -0300

    Made MainMenu and credits

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public string scene;
+    
+
+    public void LoadScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ExitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+        Debug.Log("Game Exited");
+    }
+
+    
+
+}
